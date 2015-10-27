9645abfec06202560ed8964115d46c5e0ddbd347
Matt Haggard <haggardii@gmail.com>
2015-10-27 08:20:30 -0600

    Make it installable

--- README.md
+++ README.md
@@ -2,8 +2,10 @@ This script will convert all images and CSS to data URLs.
 
 # Installation
 
-    pip install -r requirements.txt
+    pip install git+https://github.com/iffy/inliner.git
 
 # Usage
 
-    cat some.html | python inliner.py > standalone.html
+    cat some.html | inliner.py > standalone.html
+
+See `inliner.py --help` for more information.
---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# Copyright (c) Matt Haggard
+# See LICENSE for details.
+
+
+from distutils.core import setup
+
+setup(
+    name='inliner',
+    version='0.1.0',
+    description='Inlines external HTML resources',
+    author='Matt Haggard',
+    author_email='haggardii@gmail.com',
+    url='https://github.com/iffy/inliner',
+    packages=[],
+    install_requires=[
+        'lxml',
+        'requests',
+        'BeautifulSoup',
+    ],
+    scripts=[
+        'inliner.py',
+    ]
+)
